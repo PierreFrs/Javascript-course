e37479eee95cbee1bb39dd9a912f23a0ad36e2d6
Pierre <pierrefraisse90@gmail.com>
2023-05-25 14:16:30 +0200

    modules

--- app.js
+++
@@ -1,9 +0,0 @@
-// Array includes() - checks if the item is an array
-// useful in the conditional statements
-
-const groceries = ["milk", "bread", "meat"];
-
-let randomItem = "lemon";
-
-const isIncluded = groceries.includes(randomItem);
-console.log(isIncluded);
