df9842fc098950778aebc73c613c8e11d25c8700
Pierre <pierrefraisse90@gmail.com>
2023-05-25 14:16:44 +0200

    modules

---
+++ modules/get-element.js
@@ -0,0 +1,12 @@
+// global name space / hard to navigate
+
+import { people } from "./utils/data.js";
+import showPeople from "./utils/showPeople.js";
+import get from "./utils/getElement.js";
+
+const container = get(".container");
+const btn = get(".btn");
+
+btn.addEventListener("click", () => {
+  container.innerHTML = showPeople(people);
+});
---
+++ modules/utils/data.js
@@ -0,0 +1,6 @@
+// Named export
+export const people = [
+  { name: "john", job: "developer" },
+  { name: "susan", job: "designer" },
+  { name: "anna", job: "boss" },
+];
